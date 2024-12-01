276f1e1e4f05fb72a807e60ec78d673ab4096acc
Marc-Bouche <mbouch17@gmail.com>
2024-12-01 22:15:13 +0100

    function.py upload
    function.py upload

---
+++ function.py
@@ -0,0 +1,39 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "cd044021-0714-4188-8169-ad86c3ed4a24",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "def square(x):\n",
+    "    return x**2\n",
+    "\n",
+    "def say_hello(name)\n",
+    "    return f'hello dear{name}!'"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.12.4"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
